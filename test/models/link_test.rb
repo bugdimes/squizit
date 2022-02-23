6f1296a6ac197e5a8c0154a7390a51d9715fb406
Divyesh Patel <pateldivyesh009@gmail.com>
2022-02-23 23:20:53 +0530

    tests: Link Model

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid url' do
+    link = Link.new(url: 'http://www.google.com')
+    assert link.valid?
+  end
+
+  test 'invalid url' do
+    link = Link.new(url: 'www.test.   com')
+    refute link.valid?
+  end
+
+  test 'url presence' do
+    link = Link.new(url: nil)
+    refute link.save
+  end
+
+  test 'slug presence' do
+    link = Link.create(url: 'http://www.testing.com/about-us')
+    assert_not_empty link.slug
+  end
 end
